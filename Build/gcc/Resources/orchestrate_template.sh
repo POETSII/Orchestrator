2a304c5ca87dfcb25bb10509efc5cec9156317a5
mvousden <mlvousden@gmail.com>
2018-11-28 10:27:03 +0000

    Tentatively support running on non-POETS boxes.

--- Build/gcc/Resources/orchestrate_template.sh
+++ Build/gcc/Resources/orchestrate_template.sh
@@ -12,11 +12,23 @@
 # substitute in variable names to create the executable. There should be no
 # double-curly braces in the output version of this file.
 
+# Determine whether or not we are running on a POETS box, trivially.
+QUARTUS_SETUP_SCRIPT="/local/ecad/setup-quartus17v0.bash"
+if [ -f "$QUARTUS_SETUP_SCRIPT" ]; then
+    ON_POETS_BOX=1  # True
+else
+    ON_POETS_BOX=0
+fi
+
+# Setup
 export RISCV_DIR=
 export PATH="{{ MPICH_DIR }}/bin:$RISCV_DIR/bin:$PATH"
 export TRIVIAL_LOG_HANDLER=1
-export LM_LICENSE_FILE=:27012@localhost:27001@localhost
-source "/local/ecad/setup-quartus17v0.bash"
+
+if [ $ON_POETS_BOX -eq 1 ]; then
+    export LM_LICENSE_FILE=:27012@localhost:27001@localhost
+    source "$QUARTUS_SETUP_SCRIPT"
+fi
 
 # Paths for dynamically-linked libraries.
 MPI_LIB_DIR="{{ MPICH_LIB_DIR }}"
@@ -27,11 +39,18 @@ GCC_LIB_DIR="{{ GCC_LIB_DIR }}"
 INTERNAL_LIB_PATH=./:"$QT_LIB_DIR":"$MPI_LIB_DIR":\
 "$GCC_LIB_DIR":"$SUPERVISOR_LIB_DIR":"$JTAG_LIB_DIR"
 
-# Running the Orchestrator from the build directory.
-pushd "{{ EXECUTABLE_DIR }}" > /dev/null
-mpiexec.hydra -genv LD_LIBRARY_PATH "$INTERNAL_LIB_PATH" \
+# Define general MPI execution command.
+COMMAND='mpiexec.hydra -genv LD_LIBRARY_PATH "$INTERNAL_LIB_PATH" \
     -n 1 ./root : \
     -n 1 ./logserver : \
-    -n 1 ./rtcl : \
-    -n 1 ./mothership
+    -n 1 ./rtcl :'
+
+# Running the Orchestrator from the build directory. Only start a mothership if
+# we are running on a POETS box.
+pushd "{{ EXECUTABLE_DIR }}" > /dev/null
+if [ $ON_POETS_BOX -eq 1 ]; then
+    echo "$COMMAND -n 1 ./mothership"
+else
+    echo "$COMMAND"
+fi
 popd > /dev/null
