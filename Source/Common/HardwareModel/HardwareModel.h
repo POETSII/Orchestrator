9cf444cf99469e7a4d59196c1e908b34e52b04f6
mvousden <mlvousden@gmail.com>
2019-01-02 11:04:56 +0000

    Encapsulate the hardware model constituents into a single header, for convenience.

---
+++ Source/Common/HardwareModel/HardwareModel.h
@@ -0,0 +1,13 @@
+/* This header file includes a series of header files for the hardware model,
+   and exists as a convenience mechanism for sources that require the entire
+   model.
+
+   There are no header guards in this header file, because the "lower-level"
+   headers themselves have guards. */
+
+#include "PoetsEngine.h"
+#include "PoetsBox.h"
+#include "PoetsBoard.h"
+#include "PoetsMailbox.h"
+#include "PoetsCore.h"
+#include "PoetsThread.h"
