32e51741d39a6e22f08351b6ea39bc8a9247572e
mvousden <mark.vousden@soton.ac.uk>
2019-01-17 13:30:29 +0000

    Remove unused variable p0.

--- Source/OrchBase/OrchBaseLink.cpp
+++ Source/OrchBase/OrchBaseLink.cpp
@@ -206,7 +206,6 @@ vector<string> vs0 = pD->NSGetinpn();
 vector<string> vs1 = pD->NSGetinpt();
 vector<string> vs2 = pD->NSGetoupn();
 vector<string> ns3 = pD->NSGetoupt();
-P_addr_t p0 = pD->addr;
 
   ns.PutD(pD->Name(),                  // Device name
           pD->pP_devtyp->Name(),       // Device type
