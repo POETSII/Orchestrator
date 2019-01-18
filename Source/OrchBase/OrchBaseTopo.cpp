401ddcaa388aa9bcbbdb7928d584a71cbb47968f
mvousden <mark.vousden@soton.ac.uk>
2019-01-18 14:42:34 +0000

    Add warning message when trying to dump a topology when none is loaded.

--- Source/OrchBase/OrchBaseTopo.cpp
+++ Source/OrchBase/OrchBaseTopo.cpp
@@ -97,7 +97,7 @@ pPlace->pCon->Constraintm[constraint] = str2uint(Cl.Pa_v[1].Val);
 
 void OrchBase::TopoDump(Cli::Cl_t Cl)
 {
-if (pE==0) return;
+if (pE==0){Post(164);return};
 if (Cl.Pa_v.empty()) pE->dump();
 WALKVECTOR(Cli::Pa_t,Cl.Pa_v,i) {      // Loop through streams to dump to
   string st = (*i).Val;                // Dump stream
