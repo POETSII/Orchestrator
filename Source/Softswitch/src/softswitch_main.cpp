d0a679a073bc94ea70c7496afd72845a8c704374
Graeme Bragg <g.bragg@ecs.soton.ac.uk>
2019-07-16 11:36:20 +0100

    CHange execution order in softswitch main

--- Source/Softswitch/src/softswitch_main.cpp
+++ Source/Softswitch/src/softswitch_main.cpp
@@ -4,7 +4,8 @@
 void softswitch_main()
 {
     // The thread context will reside at the base of the DRAM memory area.
-    PThreadContext* ThreadContext = static_cast<PThreadContext*>(tinselHeapBase()); // softswitch_pThreadContexts + tinselID();
+    PThreadContext* ThreadContext = static_cast<PThreadContext*>(tinselHeapBase());
+    
     // can these slot assignments be done in softswitch_init?
     volatile void *recvBuffer=0;
     volatile void *sendBuffer=tinselSlot(0);   // hardware send buffer is dedicated to the first tinsel slot
@@ -14,46 +15,39 @@ void softswitch_main()
     // send a message to the local supervisor saying we are ready;
     // then wait for the __init__ message to return by interrogating tinselCanRecv(). 
     softswitch_barrier(ThreadContext, superBuffer[0], recvBuffer);
-    //softswitch_alive(superBuffer[0]); // *debug: say that we have passed initialisation*
-    /* endless main loop. It would be nice to have some stop variable here
-     * that can be set by a command. The problem is, it's not technically
-     * safe to stop until all messages that should have been expected are
-     * received. But a receiver cannot know which messages to expect, and
-     * the Supervisor can only know that all messages have been sent. There
-     * seems to be no easy way to know if all messages have been received. 
-     * Best that can be done is a command from the Supervisor that sets an
-     * end flag.
-     */
+    
+    // Endless main loop, that is until thread context says to stop.
     while (!ThreadContext->ctlEnd)
     {
-        // softswitch_alive(superBuffer[0]); // *debug: send periodic message to host*
-        // handle the receive case first as the highest priority.
-        if ((ThreadContext->receiveHasPriority || !softswitch_IsRTSReady(ThreadContext)) && tinselCanRecv())
+        // Something to receive
+        if (tinselCanRecv())
         {
-            // softswitch_alive(superBuffer[0]); // *debug: send periodic message to host*
             recvBuffer=tinselRecv();
             softswitch_onReceive(ThreadContext, recvBuffer); // decode the receive and handle
             tinselAlloc(recvBuffer); // return control of the receive buffer to the hardware
         }
-        // softswitch_IsRTSReady would be more cleanly done as a method of
-        // a class PThreadContext.
+        
+        // Something to send
         else if (softswitch_IsRTSReady(ThreadContext))
         {
-            // something to send, but channel is blocked. Do whatever idle
-            // processing can be achieved; if literally nothing can be done
-            // wait until there is something to do.
-            if (!tinselCanSend())
+            if (!tinselCanSend())   
             {
-                if (!softswitch_onIdle(ThreadContext)) tinselWaitUntil(TINSEL_CAN_SEND | TINSEL_CAN_RECV);
-            }
-            // otherwise deal with messages to send.
-            else
+               // But channel is blocked. Wait until we have something to do.
+               tinselWaitUntil(TINSEL_CAN_SEND | TINSEL_CAN_RECV);
+            } 
+            else                    
             {
-                // softswitch_alive(superBuffer[0]); // *debug: send periodic message to host*
+                // Let's send something.
                 softswitch_onSend(ThreadContext, sendBuffer);
             }
         }
-        else if (!softswitch_onIdle(ThreadContext)) tinselWaitUntil(TINSEL_CAN_RECV); // thread is idle.
+        
+        // Nothing to RX, nothing to TX: iterate through all devices until 
+        // something happens or all OnComputes have returned 0. 
+        else if(!softswitch_onIdle(ThreadContext)) 
+        {                                           
+            tinselWaitUntil(TINSEL_CAN_RECV);
+        }
     }
     softswitch_finalize(ThreadContext, &sendBuffer, &recvBuffer, superBuffer); // shut down once the end signal has been received.
 }
