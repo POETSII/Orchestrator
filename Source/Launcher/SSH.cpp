b5dd9b8d034c10c3186e70de32ffef13fa86cb39
mvousden <mlvousden@gmail.com>
2019-08-15 12:44:32 +0100

    Beginnings of SSH::call.

---
+++ Source/Launcher/SSH.cpp
@@ -0,0 +1,20 @@
+#include "SSH.h"
+
+/* Runs an SSH command, returning the exit code.
+ *
+ * This function opens a single SSH session, and runs commands natively in the
+ * shell configured in the SSH config file, falling back to the SHELL
+ * environment variable on the host (usually /bin/bash). Arguments:
+ *
+ * - host: host to connect to (see SSH.h).
+ * - command: command to run, passed naively to the shell. Each command runs
+ *   under the same SSH connection.
+ * - stdout: string populated with the contents of the standard output.
+ * - error: string populated with the contents of the standard error, or other
+     connection errors. */
+int SSH::call(std::string host, std::string command, std::string* stdout,
+              std::string* stderr);
+{
+    stderr.clear();
+    stdout.clear();
+}
