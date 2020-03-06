ab3127077868e5e51f83992453bb9f1fde77c0f0
Mark Vousden <m.vousden@soton.ac.uk>
2020-03-06 17:13:10 +0000

    The beginnings of threadcomms.

---
+++ Source/Mothership/ThreadComms.cpp
@@ -0,0 +1,267 @@
+#include "ThreadComms.h"
+
+ThreadComms::ThreadComms(Mothership* mothership):
+    mothership(mothership)
+{
+    quit = false;
+
+    /* Initialise mutexes. */
+    pthread_mutex_init(&mutex_MPI_cnc_queue);
+    pthread_mutex_init(&mutex_MPI_app_queue);
+    pthread_mutex_init(&mutex_backend_output_queue);
+}
+
+ThreadComms::~ThreadComms()
+{
+    /* Tear down mutexes. */
+    pthread_mutex_destroy(&mutex_MPI_cnc_queue);
+    pthread_mutex_destroy(&mutex_MPI_app_queue);
+    pthread_mutex_destroy(&mutex_backend_output_queue);
+}
+
+/* Code repetition ahoy! I would use the preprocessor to generate this, but
+ * then it would be too hard for your mere-mortal eyes to read. */
+
+/* ===== MPICncQueue ======================================================= */
+
+/* If there are messages in the queue, grabs the next message from MPICncQueue,
+ * writes message with it, and returns true. Otherwise, returns false. */
+bool ThreadComms::pop_MPI_cnc_queue(PMsg_p* message)
+{
+    if (MPICncQueue.empty()) return false;
+    pthread_mutex_lock(&mutex_MPI_cnc_queue);
+    *message = MPICncQueue.front();
+    MPICncQueue.pop();
+    pthread_mutex_unlock(&mutex_MPI_cnc_queue);
+    return true;
+}
+
+/* If there are messages in the queue, grabs all messages from MPICncQueue,
+ * writes them to messages, and returns true. Otherwise, returns false. */
+bool ThreadComms::pop_MPI_cnc_queue(std::vector<PMsg_p>* messages)
+{
+    if (MPICncQueue.empty()) return false;
+    pthread_mutex_lock(&mutex_MPI_app_queue);
+    messages->clear();
+    while (!MPICncQueue.empty())
+    {
+        messages->push_back(MPICncQueue.front());
+        MPICncQueue.pop();
+    }
+    pthread_mutex_unlock(&mutex_MPI_app_queue);
+    return true;
+}
+
+/* Takes message and places it into the queue. */
+bool ThreadComms::push_MPI_cnc_queue(PMsg_p message)
+{
+    pthread_mutex_lock(&mutex_MPI_cnc_queue);
+    MPICncQueue.push(message);
+    pthread_mutex_unlock(&mutex_MPI_cnc_queue);
+}
+
+/* Takes all messages and pushes them into the queue in the order in which they
+ * are stored in the vector. */
+bool ThreadComms::push_MPI_cnc_queue(std::vector<PMsg_p>* messages)
+{
+    pthread_mutex_lock(&mutex_MPI_cnc_queue);
+    for (std::vector<PMsg_p>::iterator message = messages.begin();
+         message != messages.end(); message++)
+    {
+        MPICncQueue.push(message);
+    }
+    pthread_mutex_unlock(&mutex_MPI_cnc_queue);
+}
+
+/* ===== MPIAppQueue ======================================================= */
+
+/* If there are messages in the queue, grabs the next message from MPIAppQueue,
+ * writes message with it, and returns true. Otherwise, returns false. */
+bool ThreadComms::pop_MPI_app_queue(PMsg_p* message)
+{
+    if (MPIAppQueue.empty()) return false;
+    pthread_mutex_lock(&mutex_MPI_app_queue);
+    *message = MPIAppQueue.front();
+    MPIAppQueue.pop();
+    pthread_mutex_unlock(&mutex_MPI_app_queue);
+    return true;
+}
+
+/* If there are messages in the queue, grabs all messages from MPIAppQueue,
+ * writes them to messages, and returns true. Otherwise, returns false. */
+bool ThreadComms::pop_MPI_app_queue(std::vector<PMsg_p>* messages)
+{
+    if (MPIAppQueue.empty()) return false;
+    pthread_mutex_lock(&mutex_MPI_app_queue);
+    messages->clear();
+    while (!MPIAppQueue.empty())
+    {
+        messages->push_back(MPIAppQueue.front());
+        MPIAppQueue.pop();
+    }
+    pthread_mutex_unlock(&mutex_MPI_app_queue);
+    return true;
+}
+
+/* Takes message and places it into the queue. */
+bool ThreadComms::push_MPI_app_queue(PMsg_p message)
+{
+    pthread_mutex_lock(&mutex_MPI_app_queue);
+    MPIAppQueue.push(message);
+    pthread_mutex_unlock(&mutex_MPI_app_queue);
+}
+
+/* Takes all messages and pushes them into the queue in the order in which they
+ * are stored in the vector. */
+bool ThreadComms::push_MPI_app_queue(std::vector<PMsg_p>* messages)
+{
+    pthread_mutex_lock(&mutex_MPI_app_queue);
+    for (std::vector<PMsg_p>::iterator message = messages.begin();
+         message != messages.end(); message++)
+    {
+        MPIAppQueue.push(message);
+    }
+    pthread_mutex_unlock(&mutex_MPI_app_queue);
+}
+
+/* ===== BackendOutputQueue ================================================ */
+
+/* If there are packets in the queue, grabs the next packet from
+ * BackendOutputQueue, writes packet with it, and returns true. Otherwise,
+ * returns false. */
+bool ThreadComms::pop_backend_out_queue(P_Pkt_t* packet)
+{
+    if (BackendOutputQueue.empty()) return false;
+    pthread_mutex_lock(&mutex_backend_out_queue);
+    *packet = BackendOutputQueue.front();
+    BackendOutputQueue.pop();
+    pthread_mutex_unlock(&mutex_backend_out_queue);
+    return true;
+}
+
+/* If there are packets in the queue, grabs all packets from
+ * BackendOutputQueue, writes them to packets, and returns true. Otherwise,
+ * returns false. */
+bool ThreadComms::pop_backend_out_queue(std::vector<P_Pkt_t>* packets)
+{
+    if (BackendOutputQueue.empty()) return false;
+    pthread_mutex_lock(&mutex_backend_out_queue);
+    packets->clear();
+    while (!BackendOutputQueue.empty())
+    {
+        packets->push_back(BackendOutputQueue.front());
+        BackendOutputQueue.pop();
+    }
+    pthread_mutex_unlock(&mutex_backend_out_queue);
+    return true;
+}
+
+/* Takes packet and places it into the queue. */
+bool ThreadComms::push_backend_out_queue(P_Pkt_t packet)
+{
+    pthread_mutex_lock(&mutex_backend_out_queue);
+    BackendOutputQueue.push(packet);
+    pthread_mutex_unlock(&mutex_backend_out_queue);
+}
+
+/* Takes all packets and pushes them into the queue in the order in which they
+ * are stored in the vector. */
+bool ThreadComms::push_backend_out_queue(std::vector<P_Pkt_t>* packets)
+{
+    pthread_mutex_lock(&mutex_backend_out_queue);
+    for (std::vector<P_Pkt_t>::iterator packet = packets.begin();
+         packet != packets.end(); packet++)
+    {
+        BackendOutputQueue.push(packet);
+    }
+    pthread_mutex_unlock(&mutex_backend_out_queue);
+}
+
+/* ===== BackendInputQueue ================================================= */
+
+/* If there are packets in the queue, grabs the next packet from
+ * BackendInputQueue, writes packet with it, and returns true. Otherwise,
+ * returns false. */
+bool ThreadComms::pop_backend_in_queue(P_Pkt_t* packet)
+{
+    if (BackendInputQueue.empty()) return false;
+    *packet = BackendInputQueue.front();
+    BackendInputQueue.pop();
+    return true;
+}
+
+/* If there are packets in the queue, grabs all packets from
+ * BackendInputQueue, writes them to packets, and returns true. Otherwise,
+ * returns false. */
+bool ThreadComms::pop_backend_in_queue(std::vector<P_Pkt_t>* packets)
+{
+    if (BackendInputQueue.empty()) return false;
+    packets->clear();
+    while (!BackendInputQueue.empty())
+    {
+        packets->push_back(BackendInputQueue.front());
+        BackendInputQueue.pop();
+    }
+    return true;
+}
+
+/* Takes packet and places it into the queue. */
+bool ThreadComms::push_backend_in_queue(P_Pkt_t packet)
+{
+    BackendInputQueue.push(packet);
+}
+
+/* Takes all packets and pushes them into the queue in the order in which they
+ * are stored in the vector. */
+bool ThreadComms::push_backend_in_queue(std::vector<P_Pkt_t>* packets)
+{
+    for (std::vector<P_Pkt_t>::iterator packet = packets.begin();
+         packet != packets.end(); packet++)
+    {
+        BackendInputQueue.push(packet);
+    }
+}
+
+/* ===== DebugInputQueue =================================================== */
+
+/* If there are packets in the queue, grabs the next packet from
+ * DebugInputQueue, writes packet with it, and returns true. Otherwise, returns
+ * false. */
+bool ThreadComms::pop_debug_in_queue(P_Pkt_t* packet)
+{
+    if (DebugInputQueue.empty()) return false;
+    *packet = DebugInputQueue.front();
+    DebugInputQueue.pop();
+    return true;
+}
+
+/* If there are packets in the queue, grabs all packets from DebugInputQueue,
+ * writes them to packets, and returns true. Otherwise, returns false. */
+bool ThreadComms::pop_debug_in_queue(std::vector<P_Pkt_t>* packets)
+{
+    if (DebugInputQueue.empty()) return false;
+    packets->clear();
+    while (!DebugInputQueue.empty())
+    {
+        packets->push_back(DebugInputQueue.front());
+        DebugInputQueue.pop();
+    }
+    return true;
+}
+
+/* Takes packet and places it into the queue. */
+bool ThreadComms::push_debug_in_queue(P_Pkt_t packet)
+{
+    DebugInputQueue.push(packet);
+}
+
+/* Takes all packets and pushes them into the queue in the order in which they
+ * are stored in the vector. */
+bool ThreadComms::push_debug_in_queue(std::vector<P_Pkt_t>* packets)
+{
+    for (std::vector<P_Pkt_t>::iterator packet = packets.begin();
+         packet != packets.end(); packet++)
+    {
+        DebugInputQueue.push(packet);
+    }
+}
---
+++ Source/Mothership/ThreadComms.h
@@ -0,0 +1,91 @@
+#ifndef __ORCHESTRATOR_SOURCE_MOTHERSHIP_THREADCOMMS_H
+#define __ORCHESTRATOR_SOURCE_MOTHERSHIP_THREADCOMMS_H
+
+/* Describes how the different threads of execution in the Mothership interact,
+ * and defines queue and pthread constructs for the Mothership. The
+ * documentation is pretty good at describing it - you should go read it if you
+ * haven't already. */
+
+#include <pthread.h>
+#include <queue>
+
+#include "Mothership.h"
+#include "poets_pkt.h"
+#include "PMsg_p.h"
+
+/* Used to declare some methods. */
+#define START_THREAD_METHOD_DECLARATIONS(THREAD) \
+    void start_THREAD(); \
+    void* THREAD(Mothership*);
+
+class ThreadComms
+{
+public:
+    ThreadComms(Mothership*);
+    ~ThreadComms();
+    void go();
+
+    /* Quitting logic. */
+    inline void set_quit(){quit = true;};
+    inline bool is_it_time_to_go(){return quit;};
+
+    /* Queue and mutex manipulation */
+    bool pop_MPI_cnc_queue(PMsg_p*);
+    bool pop_MPI_cnc_queue(std::vector<PMsg_p>*);
+    void push_MPI_cnc_queue(PMsg_p);
+    void push_MPI_cnc_queue(std::vector<PMsg_p>*);
+
+    bool pop_MPI_app_queue(PMsg_p*);
+    bool pop_MPI_app_queue(std::vector<PMsg_p>*);
+    void push_MPI_app_queue(PMsg_p);
+    void push_MPI_app_queue(std::vector<PMsg_p>*);
+
+    bool pop_backend_out_queue(P_Pkt_t*);
+    bool pop_backend_out_queue(std::vector<P_Pkt_t>*);
+    void push_backend_out_queue(P_Pkt_t);
+    void push_backend_out_queue(std::vector<P_Pkt_t>*);
+
+    bool pop_backend_in_queue(P_Pkt_t*);
+    bool pop_backend_in_queue(std::vector<P_Pkt_t>*);
+    void push_backend_in_queue(P_Pkt_t);
+    void push_backend_in_queue(std::vector<P_Pkt_t>*);
+
+    bool pop_debug_in_queue(P_Pkt_t*);
+    bool pop_debug_in_queue(std::vector<P_Pkt_t>*);
+    void push_debug_in_queue(P_Pkt_t);
+    void push_debug_in_queue(std::vector<P_Pkt_t>*);
+
+private:
+    Mothership* mothership;
+    bool quit;
+
+    /* Mutexes */
+    pthread_mutex_t mutex_MPI_cnc_queue;
+    pthread_mutex_t mutex_MPI_app_queue;
+    pthread_mutex_t mutex_backend_output_queue;
+
+    /* Non-main "Threads" */
+    pthread_t MPIInputBroker;
+    pthread_t MPICncResolver;
+    pthread_t MPIApplicationResolver;
+    pthread_t BackendOutputBroker;
+    pthread_t BackendInputBroker;
+    pthread_t DebugInputBroker;
+
+    /* Starting threads and their methods */
+    START_THREAD_METHOD_DECLARATIONS(mpi_input_broker)
+    START_THREAD_METHOD_DECLARATIONS(mpi_cnc_resolver)
+    START_THREAD_METHOD_DECLARATIONS(mpi_application_resolver)
+    START_THREAD_METHOD_DECLARATIONS(backend_output_broker)
+    START_THREAD_METHOD_DECLARATIONS(backend_input_broker)
+    START_THREAD_METHOD_DECLARATIONS(debug_input_broker)
+
+    /* Queues */
+    std::queue<PMsg_p> MPICncQueue;
+    std::queue<PMsg_p> MPIAppQueue;
+    std::queue<P_Pkt_t> BackendOutputQueue;
+    std::queue<P_Pkt_t> BackendInputQueue;
+    std::queue<P_Pkt_t> DebugInputQueue;
+};
+
+#endif
