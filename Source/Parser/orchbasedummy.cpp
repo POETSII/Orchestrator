98f2d09c5755cdd150c53764fc5f21bdf60ab473
Graeme Bragg <helios@m0nsa.com>
2020-09-17 12:06:34 +0100

    Tidy up playpen and parser, which are no longer used

--- Source/Parser/orchbasedummy.cpp
+++
@@ -1,8 +0,0 @@
-#include "orchbasedummy.h"
-
-OrchBaseDummy::OrchBaseDummy(int argc, char *argv[], string d, string sfile) : OrchBase(argc, argv, d, sfile)
-{
-
-}
-
-OrchBaseDummy::~OrchBaseDummy() {}
