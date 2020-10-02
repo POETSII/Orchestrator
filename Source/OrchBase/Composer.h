1d4804c8703459fd90a2615b0891da9c2d813ef9
Graeme Bragg <helios@m0nsa.com>
2020-10-02 18:04:02 +0100

    REname P_Builder to COmposer

---
+++ Source/OrchBase/Composer.h
@@ -0,0 +1,55 @@
+#ifndef __P_builderH__H
+#define __P_builderH__H
+
+#include <stdio.h>
+#include "OrchBase.h"
+#include "OSFixes.hpp"
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "build_defs.h"
+using namespace std;
+
+/* I tried */
+#define MARKTAPSOUT 1
+
+#ifndef MARKTAPSOUT
+#ifndef __BORLANDC__
+#include "i_graph.h"
+#include <QtCore>
+#endif
+#endif
+
+//==============================================================================
+
+class P_builder
+{
+public:
+                    P_builder(OrchBase *);
+virtual ~           P_builder();
+
+#ifdef MARKTAPSOUT
+
+void                Build(GraphI_t * = 0);
+void                Clear(GraphI_t * = 0);
+void                Dump(FILE * = stdout);
+void                Load(const string&);
+
+OrchBase *            par;
+
+private:
+
+unsigned GenFiles(GraphI_t*);
+unsigned CompileBins(GraphI_t*);
+
+unsigned GenSupervisor(GraphI_t*);
+unsigned WriteCoreVars(std::string&, unsigned, P_core*, ofstream&);
+unsigned WriteThreadVars(std::string&, unsigned, unsigned, P_thread*, ofstream&);
+
+#endif
+
+};
+
+//==============================================================================
+
+#endif
