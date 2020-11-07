095f69442440581190557b4c3cf548ac7d05e9a6
Mark Vousden <m.vousden@soton.ac.uk>
2020-11-07 12:13:24 +0000

    Remove SupervisorImplicitCall, and fold its logic into SupervisorCall.

--- Source/Supervisor/Supervisor.cpp
+++ Source/Supervisor/Supervisor.cpp
@@ -8,68 +8,51 @@ extern "C"
         // Call Supervisor::OnInit
         return Supervisor::OnInit()
     }
-    
-    int SupervisorImplicitCall(PMsg_p* In, PMsg_p* Out)
-    {
-#ifdef _APPLICATION_SUPERVISOR_        
-        
-        int sentErr = 0;
 
-        vector<P_Pkt_t> pkts; // packets are packed in Tinsel packet format
-        In->Put<P_Pkt_t>();
-        In->Get(1, pkts);
-        WALKVECTOR(P_Pkt_t, pkts, pkt) // and they're sent blindly
-        {
-            sentErr += Supervisor::OnImplicit(&*pkt);
-        }
-        return sentErr;
-#endif       
-        return -1;
-    }
-    
     int SupervisorCall(PMsg_p* In, PMsg_p* Out)
     {
-#ifdef _APPLICATION_SUPERVISOR_        
-        
+#ifdef _APPLICATION_SUPERVISOR_
         int sentErr = 0;
-
-        vector<P_Pkt_t> pkts; // packets are packed in Tinsel packet format
+        uint8_t opcode;
+        std::vector<P_Pkt_t> pkts;  // packets are packed in Tinsel packet
+                                    // format
         In->Put<P_Pkt_t>();
         In->Get(1, pkts);
-        WALKVECTOR(P_Pkt_t, pkts, pkt) // and they're sent blindly
+        WALKVECTOR(P_Pkt_t, pkts, pkt)
         {
-            sentErr += Supervisor::OnPkt(&*pkt);
+            opcode = (pkt->header.swAddr & P_SW_OPCODE_MASK) >>
+                P_SW_OPCODE_SHIFT;
+            if (opcode == P_CNC_IMPL) sentErr += Supervisor::OnImplicit(&*pkt);
+            else sentErr += Supervisor::OnPkt(&*pkt);
         }
         return sentErr;
-#endif       
+#endif
         return -1;
     }
-    
+
     int SupervisorIdle(){return Supervisor::OnIdle();}
-    
+
     int SupervisorCtl(){return Supervisor::OnCtl();}
-    
+
     int SupervisorRTCL(){return Supervisor::OnRTCL();}
-    
+
     int SupervisorExit(){return Supervisor::OnStop();}
-    
+
     SupervisorDeviceInstance_t SupervisorIdx2Addr(uint32_t idx)
     {
         if(idx >= Supervisor::DeviceVector.size())
         {
             //out of range Idx
-            
+
             SupervisorDeviceInstance_t fail;
             fail.HwAddr = 0;
             fail.SwAddr = 0;
             fail.Name = "";
-            
+
             return fail;
         }
-        
+
         return Supervisor::DeviceVector[idx];
     }
-    
+
 }
-
-
