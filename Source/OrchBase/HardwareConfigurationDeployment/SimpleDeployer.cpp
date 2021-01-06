28fc3c982ce21f11ad5e1afaf0f06dbe064fc8fa
Mark Vousden <m.vousden@soton.ac.uk>
2021-01-06 13:48:29 +0000

    Correct datetimes and filenames in tests.

--- Source/OrchBase/HardwareConfigurationDeployment/SimpleDeployer.cpp
+++ Source/OrchBase/HardwareConfigurationDeployment/SimpleDeployer.cpp
@@ -10,7 +10,7 @@ SimpleDeployer::SimpleDeployer()
 
     author = "Mark Vousden";
     datetime = 201901101712;
-    version = "0.3.1";
+    version = "0.5.1";
     fileOrigin = "SimpleDeployer.cpp";
 
     boxWordLength = 0;  /* There's only one box! */
