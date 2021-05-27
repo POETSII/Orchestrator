74e27478dee3e7031cda1c608e8abb1080a83756
David Thomas <dt10@imperial.ac.uk>
2021-05-27 09:04:32 +0100

    Fixes to parser to get existing v4 XML parsing.

--- Source/OrchBase/XMLProcessing/DS_XML.cpp
+++ Source/OrchBase/XMLProcessing/DS_XML.cpp
@@ -40,6 +40,7 @@ DS_map["InputPin"]         = cInputPin;
 DS_map["MessageType"]      = cMessageType;
 DS_map["MessageTypes"]     = cMessageTypes;
 DS_map["MetaData"]         = cMetaData;
+DS_map["Metadata"]         = cMetaData;
 DS_map["OnCTL"]            = cOnCTL;
 DS_map["OnDeviceIdle"]     = cOnDeviceIdle;
 DS_map["DevI"]             = cDevI;
@@ -424,6 +425,14 @@ WALKVECTOR(xnode *,pn->vnode,i) {
 PinT_t * DS_XML::_PinT_t(DevT_t * pD,xnode * pn)
 // XML::PinType
 {
+// Hack to reject indexed pins while they are not supported
+string indexed = pn->FindAttr("indexed");
+if(!indexed.empty()){
+  if(indexed!="false" && indexed!="0"){
+    par->Post(933,__FILE__,int2str(__LINE__));
+  }
+}
+
 string pname = pn->FindAttr("name");
 unsigned line = pn->lin;               // Go get file location
 PinT_t * pP;
--- Tests/ReferenceXML/test_parse.sh
+++ Tests/ReferenceXML/test_parse.sh
@@ -5,15 +5,36 @@ echo "TAP version 13"
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ORCHROOT="$(realpath $HERE/../..)"
 
+VERBOSE=1
+ABORT_ON_ERROR=1
+
 TN=0
 
 ############################################
 ## Pure syntax
 
+LOAD_PLOG_FILTER='s/^.*for the command '\''load \/app = [^'\'']+'\'' will be written to '\''([^'\'']+)'\''.+$/\1/p'
+
 function test_parse_success {
     F="$1"
-    $ORCHROOT/parse_app.exp $F > /dev/null
-    RES=$?
+    if [[ $VERBOSE -eq 1 ]] ; then
+        OUTPUT=$($ORCHROOT/parse_app.exp $F )
+        RES=$?
+
+        if [[ $RES -ne 0 ]] ; then
+            PLOG_LINE=$(echo $OUTPUT |  tr -dc '[[:print:]]' | sed -r -n "${LOAD_PLOG_FILTER}" )
+            echo ""
+            echo "#Validation of $F Failed."
+            echo "# plog is in ${PLOG_LINE}"
+            echo ""
+            while read l ; do
+                echo "# > $l"
+            done < $ORCHROOT/bin/${PLOG_LINE}
+        fi
+    else
+        $ORCHROOT/parse_app.exp $F > /dev/null
+    fi
+    
 
     RR=$(realpath --relative-to="$ORCHROOT" "$F")
 
@@ -21,6 +42,9 @@ function test_parse_success {
         echo "ok $TN - Parse $RR"
     else
         echo "not ok $TN - Parse $RR"
+        if [[ ${ABORT_ON_ERROR} -eq 1 ]] ; then
+            exit 1;
+        fi
     fi
 
     TN=$((TN+1))
@@ -53,68 +77,6 @@ for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L0-syntax/*.xml ; d
     test_parse_failure $i
 done 
 
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/apps/*.xml ; do
-    test_parse_success $i
-done 
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/valid/*/*.xml ; do
-    test_parse_success $i
-done 
-
 for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L0-syntax/*.xml ; do
-    test_parse_failure $i
-done 
-
-############################################
-## Compilation
-
-function test_compile_success {
-    F="$1"
-    $ORCHROOT/compile_app.exp $F > /dev/null
-    RES=$?
-
-    RR=$(realpath --relative-to="$ORCHROOT" "$F")
-
-    if [[ $RES -eq 0 ]] ; then
-        echo "ok $TN - Compile $RR"
-    else
-        echo "not ok $TN - Compile $RR"
-    fi
-
-    TN=$((TN+1))
-}
-
-function test_compile_failure {
-    F="$1"
-    $ORCHROOT/compile_app.exp $F > /dev/null
-    RES=$?
-
-    RR=$(realpath --relative-to="$ORCHROOT" "$F")
-
-    if [[ $RES -ne 0 ]] ; then
-        echo "ok $TN - Should fail to compile $RR"
-    else
-        echo "not ok $TN - Should fail to compile $RR"
-    fi
-
-    TN=$((TN+1))
-}
-
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/apps/*.xml ; do
-    test_compile_success $i
-done 
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/valid/*/*.xml ; do
-    test_compile_success $i
-done 
-
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L0-syntax/*.xml ; do
-    test_parse_failure $i
-done 
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L1-graph-topology/*.xml ; do
-    test_parse_failure $i
-done 
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L2-graph-attributes/*.xml ; do
-    test_parse_failure $i
-done 
-for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L3-compilation/*.xml ; do
     test_parse_failure $i
 done 
