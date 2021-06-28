4699c9a2de23a362827f51122f32ac0ab9a03fe4
Mark Vousden <m.vousden@soton.ac.uk>
2021-06-28 18:10:17 +0100

    Produce an exit code for tests that use reference XML.

--- Tests/ReferenceXML/test_parse.sh
+++ Tests/ReferenceXML/test_parse.sh
@@ -32,12 +32,12 @@ TN=0
 
 LOAD_PLOG_FILTER='s/^.*for the command '\''load \/app = [^'\'']+'\'' will be written to '\''([^'\'']+)'\''.+$/\1/p'
 
-
 function TODO_parse {
     F="$1"
     RR=$(realpath --relative-to="$ORCHROOT" "$F")
     echo "not ok $TN - # TODO Parse $RR, $2"
     TN=$((TN+1))
+    return 1
 }
 
 function test_parse_success {
@@ -60,20 +60,22 @@ function test_parse_success {
         $HERE/parse_app.exp $F > /dev/null
         RES=$?
     fi
-    
+
 
     RR=$(realpath --relative-to="$ORCHROOT" "$F")
 
     if [[ $RES -eq 0 ]] ; then
         echo "ok $TN - Parse $RR"
+        TN=$((TN+1))
+        return 0
     else
         echo "not ok $TN - Parse $RR"
+        TN=$((TN+1))
         if [[ ${ABORT_ON_ERROR} -eq 1 ]] ; then
-            exit 1;
+            exit 1
         fi
+        return 1
     fi
-
-    TN=$((TN+1))
 }
 
 function test_parse_failure {
@@ -85,28 +87,36 @@ function test_parse_failure {
 
     if [[ $RES -ne 0 ]] ; then
         echo "ok $TN - Should fail to parse $RR"
+        TN=$((TN+1))
+        return 0
     else
         echo "not ok $TN - Should fail to parse $RR"
+        TN=$((TN+1))
+        return 1
     fi
-
-    TN=$((TN+1))
 }
 
+RC=0
 for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/apps/*.xml ; do
     if [[ "$i" == *betweeness_centrality_16_16_20_20_v4.xml ]] ; then
         TODO_parse $i "orchestrator needs indexed sends."
     else
         test_parse_success $i
     fi
-done 
+    if [ $? -ne 0 ]; then RC=1; fi
+done
 for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/valid/*/*.xml ; do
     test_parse_success $i
-done 
+    if [ $? -ne 0 ]; then RC=1; fi
+done
 
 for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L0-syntax/*.xml ; do
     test_parse_failure $i
-done 
+    if [ $? -ne 0 ]; then RC=1; fi
+done
 
 for i in $ORCHROOT/Tests/ReferenceXML/v4/PEP20/tests/invalid/L0-syntax/*.xml ; do
     test_parse_failure $i
-done 
+    if [ $? -ne 0 ]; then RC=1; fi
+done
+exit $RC
