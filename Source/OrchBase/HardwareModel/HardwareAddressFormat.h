0dafaf6e771e8844dce9eee626af6dafa3879864
Mark Vousden <m.vousden@soton.ac.uk>
2021-06-29 16:02:57 +0100

    Define getters and setters for hardware address formats, and precompute maximum values.

--- Source/OrchBase/HardwareModel/HardwareAddressFormat.h
+++ Source/OrchBase/HardwareModel/HardwareAddressFormat.h
@@ -20,14 +20,38 @@ public:
                           unsigned coreWordLength,
                           unsigned threadWordLength);
     HardwareAddressFormat();
+    void Dump(FILE* = stdout);
 
+    void set_box_word_length(const unsigned value);
+    void set_board_word_length(const unsigned value);
+    void set_mailbox_word_length(const unsigned value);
+    void set_core_word_length(const unsigned value);
+    void set_thread_word_length(const unsigned value);
+    unsigned get_box_word_length();
+    unsigned get_board_word_length();
+    unsigned get_mailbox_word_length();
+    unsigned get_core_word_length();
+    unsigned get_thread_word_length();
+    unsigned get_box_max_value();
+    unsigned get_board_max_value();
+    unsigned get_mailbox_max_value();
+    unsigned get_core_max_value();
+    unsigned get_thread_max_value();
+
+private:
     unsigned boxWordLength;
     unsigned boardWordLength;
     unsigned mailboxWordLength;
     unsigned coreWordLength;
     unsigned threadWordLength;
 
-    void Dump(FILE* = stdout);
+    unsigned boxMaxValue;
+    unsigned boardMaxValue;
+    unsigned mailboxMaxValue;
+    unsigned coreMaxValue;
+    unsigned threadMaxValue;
+
+    unsigned max_from_length(const unsigned length);
 };
 
 #endif
